aa3bb17102c9357dac807b49f8c1dc2fb76b6ec7
pederengelstad <pengel@colostate.edu>
2019-08-14 12:11:01 -0600

    continued zonal updates

--- processing.R
+++ processing.R
@@ -4,8 +4,13 @@ library(tidyverse)
 # Update species name in zonal stats
 
 
-csv = as_tibble(read.table(file = "C:/Users/peder/Documents/USGS/Data/amurpeppervine/amurpeppervine_zonal.csv",
+csv = as_tibble(read.csv(file = "C:/Users/peder/Documents/USGS/Data/amurpeppervine/amurpeppervine_zonal.csv",
            header=T, sep=",")) %>%
-  select(-X) %>%
-  mutate(Species = "Amur peppervine")
-write.csv(csv, file = "C:/Users/peder/Documents/GitHub/Repositories/inhabit_sandbox/species/amurpeppervine/amurpeppervine_zonal.csv")
+  # select(-X) %>%
+#   mutate(Species = "Amur peppervine")
+# write.csv(csv, file = "C:/Users/peder/Documents/GitHub/Repositories/inhabit_sandbox/species/amurpeppervine/amurpeppervine_zonal.csv")
+
+tolower(gsub(pattern = "\\s", '', gsub("'", replacement = '', x = "Bishop's goutweed")))
+
+setwd("C:/Users/peder/Documents/GitHub/Repositories/inhabit_sandbox/species/")
+list.files(path = './amurpeppervine/', pattern = 'zonal', full.names = T)
