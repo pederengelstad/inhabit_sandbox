df9419ddd3013e158100fb74da7d67127895c005
pederengelstad <pengel@colostate.edu>
2019-08-14 11:32:49 -0600

    updating for zonal

--- processing.R
+++ processing.R
@@ -1,3 +1,11 @@
-library(rgdal)
 library(raster)
+library(rgdal)
+library(tidyverse)
+# Update species name in zonal stats
 
+
+csv = as_tibble(read.table(file = "C:/Users/peder/Documents/USGS/Data/amurpeppervine/amurpeppervine_zonal.csv",
+           header=T, sep=",")) %>%
+  select(-X) %>%
+  mutate(Species = "Amur peppervine")
+write.csv(csv, file = "C:/Users/peder/Documents/GitHub/Repositories/inhabit_sandbox/species/amurpeppervine/amurpeppervine_zonal.csv")
