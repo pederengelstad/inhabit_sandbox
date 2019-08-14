df8238558370bcca74a027a5b27797bed4e4d4d6
pederengelstad <pengel@colostate.edu>
2019-08-14 17:01:25 -0600

    multiple mods

--- processing.R
+++ processing.R
@@ -1,16 +1,42 @@
 library(raster)
 library(rgdal)
 library(tidyverse)
-# Update species name in zonal stats
+library(sp)
+library(rgeos)
+library(sf)
 
+# Make sure to polygonize FDAWs first...
 
-csv = as_tibble(read.csv(file = "C:/Users/peder/Documents/USGS/Data/amurpeppervine/amurpeppervine_zonal.csv",
-           header=T, sep=",")) %>%
-  # select(-X) %>%
-#   mutate(Species = "Amur peppervine")
-# write.csv(csv, file = "C:/Users/peder/Documents/GitHub/Repositories/inhabit_sandbox/species/amurpeppervine/amurpeppervine_zonal.csv")
+# List species folders
+dirs = list.dirs(path = "C:/Users/peder/Documents/USGS/Data/20190723", full.names = T, recursive = F)
+conus = readOGR("C:/Users/peder/Documents/GitHub/Repositories/inhabit_sandbox/CONUS_4269/CONUS_4269.shp")
 
-tolower(gsub(pattern = "\\s", '', gsub("'", replacement = '', x = "Bishop's goutweed")))
-
-setwd("C:/Users/peder/Documents/GitHub/Repositories/inhabit_sandbox/species/")
-list.files(path = './amurpeppervine/', pattern = 'zonal', full.names = T)
+for(d in dirs){
+  
+  # Process FDAW
+  kde = readOGR(list.files(d, pattern = 'kde_tmp.shp', full.names = T))
+  kde = spTransform(kde, CRSobj = crs(shp))
+  clip = rgeos::gIntersection(kde, conus)
+  clip = SpatialPolygonsDataFrame(Sr = clip, data = data.frame(data = 1))
+  writeOGR(obj = clip, 
+           layer = "kde", 
+           driver = "ESRI Shapefile", 
+           dsn = paste0(d,'/kde.shp'))
+  
+  # Clean things up
+  if(file.exists(paste0(d, '/kde.shp'))) unlink(list.files(d, pattern = 'kde_tmp*', full.names = T))
+  
+  # Convert presence points to shapefile
+  csv = read.table(file = list.files(d, pattern = 'csv', full.names = T), 
+                   header = T, sep = ',', stringsAsFactors = F) %>%
+    filter(responseBinary == 1) %>%
+    select(X, Y) %>%
+    mutate(X = as.numeric(X), Y = as.numeric(Y))
+  
+  pts = SpatialPointsDataFrame(coords = csv, data = csv, 
+                               proj4string = crs('+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs)'))
+  
+  pts = spTransform(pts, CRSobj = crs(shp))
+  pts.out = sf::st_intersection(sf::st_as_sf(pts), sf::st_as_sf(clip))
+  st_write(pts.out, dsn = paste0(d, "/pts.sqlite"), layer = "pts")
+}
